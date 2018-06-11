241de2fc74622c73a59309d2ef323ab81ea362bc
HerbertY <10234670@qq.com>
2018-06-11 23:26:24 +0800

    UI层级排序

---
+++ Scripts/Utils/UILayerOrder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// UI层级排序       by:heyang 2018/6/11
+/// </summary>
+public class UILayerOrder : MonoBehaviour {
+    [SerializeField, SetProperty("Order")]
+    private int order = 0;
+    public int Order
+    {
+        get
+        {
+            return order;
+        }
+        set
+        {
+            order = value;
+            SetLayerOrder();
+        }
+    }
+
+    /// <summary>
+    /// 设置层级
+    /// </summary>
+    private void SetLayerOrder()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        if (canvas == null)
+        {
+            canvas = gameObject.AddComponent<Canvas>();
+        }
+
+        canvas.overrideSorting = true;
+        canvas.sortingOrder = order;
+    }
+}
